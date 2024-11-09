bda27430ca04e02241ac43b9f315e4c7b7e263d5
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-11-09 13:59:41 +0000

    started project

---
+++ src/app/views/main/timelogs/timelogs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timelogs',
+  standalone: true,
+  imports: [],
+  templateUrl: './timelogs.component.html',
+  styleUrl: './timelogs.component.scss'
+})
+export class TimelogsComponent {
+
+}
