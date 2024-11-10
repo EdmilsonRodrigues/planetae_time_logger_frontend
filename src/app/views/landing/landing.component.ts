98b1898695d966be207a42fcb435228203992375
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-11-10 13:37:40 +0000

    Started Creation of index template

--- src/app/views/landing/landing.component.ts
+++ src/app/views/landing/landing.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LandingHeaderComponent } from "./header/header.component";
+import { LandingMainComponent } from "./main/main.component";
+import { LandingFooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [LandingHeaderComponent, LandingMainComponent, LandingFooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
